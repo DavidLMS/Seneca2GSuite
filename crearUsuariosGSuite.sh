85cb85f14a9c66ffdd7a31a35c29a96db5922f88
DavidLMS <lms.davidromero@gmail.com>
2019-06-08 10:15:23 +0200

    Update crearUsuariosGSuite.sh

--- crearUsuariosGSuite.sh
+++ crearUsuariosGSuite.sh
@@ -3,13 +3,15 @@
 DOMINIO="@lasallemundonuevo.es"
 #Unimos los .csv de los distintos cursos que haya en la carpeta (regAlum de Séneca)
 cat *.csv > ficheros-unidos.csv
+#Insertamos \n porque de lo contrario no lee el último alumno
+echo '\n' >> ficheros-unidos.csv
 #Escribimos la contraseña que deseamos que tengan todas las nuevas cuentas creadas
 echo 'Introduzca la contraseña que desea asignar a las cuentas:'
 read password
 #Convertimos el fichero al formato UTF-8
 iconv -f "windows-1252" -t "UTF-8" ficheros-unidos.csv > ficheros-unidos-utf8.csv
 IFS=,
-while read apellidos nombre resto
+while read apellidos nombre resto;
 do
 	echo "nombre: ${nombre/\"/}"
 	echo "apellidos: ${apellidos/\"/}"
@@ -18,3 +20,6 @@ do
 	echo "email: $email"
 	./gam create user $email firstname "${nombre/\"/}" lastname "${apellidos/\"/}" password $password
 done < ficheros-unidos-utf8.csv
+#Eliminamos los ficheros auxiliares utilizados
+rm ficheros-unidos.csv
+rm ficheros-unidos-utf8.csv
