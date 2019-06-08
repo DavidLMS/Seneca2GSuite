685097fa4208bfe3e8a6435cc6c8861e46f7d783
DavidLMS <lms.davidromero@gmail.com>
2019-06-08 09:43:51 +0200

    Update crearUsuariosGSuite.sh

--- crearUsuariosGSuite.sh
+++ crearUsuariosGSuite.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+#En esta variable ponemos nuestro dominio educativo (incluyendo la @)
+DOMINIO = @dominio.com
+#Unimos los .csv de los distintos cursos que haya en la carpeta (regAlum de Séneca)
 cat *.csv > ficheros-unidos.csv
+#Escribimos la contraseña que deseamos que tengan todas las nuevas cuentas creadas
 echo 'Introduzca la contraseña que desea asignar a las cuentas:'
 read password
+#Convertimos el fichero al formato UTF-8
 iconv -f "windows-1252" -t "UTF-8" ficheros-unidos.csv > ficheros-unidos-utf8.csv
 IFS=,
 while read apellidos nombre resto
 do
 	echo "nombre: ${nombre/\"/}"
 	echo "apellidos: ${apellidos/\"/}"
-	email=$(echo "${nombre/\"/}${apellidos/\"/}""@lasallemundonuevo.es" | tr '[:upper:]' '[:lower:]' | tr -d '[[:space:]]' | tr "áéíóúñ" "aeioun")
+	#El email estará formado por el nombre y dos apellidos seguidos
+	email=$(echo "${nombre/\"/}${apellidos/\"/}"$DOMINIO | tr '[:upper:]' '[:lower:]' | tr -d '[[:space:]]' | tr "áéíóúñ" "aeioun")
 	echo "email: $email"
-	/Users/david/bin/gam/gam create user $email firstname "${nombre/\"/}" lastname "${apellidos/\"/}" password $password
+	gam create user $email firstname "${nombre/\"/}" lastname "${apellidos/\"/}" password $password
 done < ficheros-unidos-utf8.csv
