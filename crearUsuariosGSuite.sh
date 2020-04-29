3a326ce6ed3e54842ec95d34085c9dd9796ca4fd
DavidLMS <lms.davidromero@gmail.com>
2020-04-29 07:40:50 +0200

    Añadida opción de forzar o no el cambio de contraseña

--- crearUsuariosGSuite.sh
+++ crearUsuariosGSuite.sh
@@ -8,6 +8,9 @@ echo '\n' >> ficheros-unidos.csv
 #Escribimos la contraseña que deseamos que tengan todas las nuevas cuentas creadas
 echo 'Introduzca la contraseña que desea asignar a las cuentas:'
 read password
+#Escribimos si deseamos o no forzar un cambio de contraseña al primer inicio de sesión
+echo 'Escribe "on" si desea forzar un cambio de contraseña al iniciar sesión o escribe "off" si no lo desea'
+read force
 #Convertimos el fichero al formato UTF-8
 iconv -f "windows-1252" -t "UTF-8" ficheros-unidos.csv > ficheros-unidos-utf8.csv
 IFS=,
@@ -16,9 +19,9 @@ do
 	echo "nombre: ${nombre/\"/}"
 	echo "apellidos: ${apellidos/\"/}"
 	#El email estará formado por el nombre y dos apellidos seguidos
-	email=$(echo "${nombre/\"/}${apellidos/\"/}"$DOMINIO | tr '[:upper:]' '[:lower:]' | tr -d '[[:space:]]' | tr "áéíóúñ" "aeioun")
+	email=$(echo "${nombre/\"/}${apellidos/\"/}"$DOMINIO | tr '[:upper:]' '[:lower:]' | tr -d '[[:space:]]' | tr "áéíóúñü" "aeiounu")
 	echo "email: $email"
-	./gam create user $email firstname "${nombre/\"/}" lastname "${apellidos/\"/}" password $password
+	./gam create user $email firstname "${nombre/\"/}" lastname "${apellidos/\"/}" password $password changepassword $force
 done < ficheros-unidos-utf8.csv
 #Eliminamos los ficheros auxiliares utilizados
 rm ficheros-unidos.csv
